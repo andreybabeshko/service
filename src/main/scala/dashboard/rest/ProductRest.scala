bf4aaab7eccc32ae0ee55dc03ffe246ed46e3a64
alexander.nikulin <alexander.nikulin@techinsight.ua>
2015-10-30 18:15:37 +0200

    #265 // and flag store

--- src/main/scala/dashboard/rest/ProductRest.scala
+++ src/main/scala/dashboard/rest/ProductRest.scala
@@ -15,16 +15,16 @@ object ProductRest extends BaseRestApi {
 
     case JsonGet("prices" :: product :: Nil, _) =>
       serialize(ProductService.searchProductsWithPrices(product.replaceAll("%20", " "))
-        .map(p => new ResponseProductPrice(p._1, "", p._2)))
+        .map(p => new ResponseProductPrice(p._1, true, p._2)))
 
     case JsonGet("prices" :: product :: store ::Nil, _) =>
       serialize(ProductService.searchProductsWithPrices(product.replaceAll("%20", " "), store.replaceAll("%20", " "))
-        .map(p => new ResponseProductPrice(p._1, "", p._2)))
+        .map(p => new ResponseProductPrice(p._1, false, p._2)))
   })
 
   case class ResponseProductPrice (
    name:  String,
-   store: String,
+   store: Boolean,
    price: Seq[Seq[Any]]
   )
 
